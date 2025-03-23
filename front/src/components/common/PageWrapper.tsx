3b9f20381d7c20dc67f2fe84736472543d695993
fberger <frcs.berger@gmail.com>
2025-03-23 22:26:20 +0100

    front(all): wip

--- front/src/components/common/PageWrapper.tsx
+++ front/src/components/common/PageWrapper.tsx
@@ -1,8 +1,7 @@
 import { cn } from '@/utils'
 
 export default function PageWrapper({ children, className, ...props }: { children: React.ReactNode; className?: string; showQuote?: boolean }) {
-    const commonClassName =
-        'mx-auto mt-[20px] flex min-h-screen w-full max-w-screen flex-col items-start gap-10 overflow-auto px-4 pb-4 sm:mt-12 md:px-8'
+    const commonClassName = 'mx-auto flex min-h-screen w-full max-w-screen flex-col items-start gap-10 overflow-auto px-4 pb-4 md:px-8'
     return (
         <div {...props} className={cn(commonClassName, className)}>
             {children}
