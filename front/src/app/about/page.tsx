5da53ca812839adb0594972b9f4f1de7704c541a
fberger <frcs.berger@gmail.com>
2025-05-15 15:45:31 +0200

    feat: about page

--- front/src/app/about/page.tsx
+++ front/src/app/about/page.tsx
@@ -30,9 +30,9 @@ export default function Page() {
         <PageWrapper className="mt-14">
             <div className="flex flex-col w-full gap-24 items-center">
                 {/* tagline */}
-                <div className="flex max-w-[520px] flex-col gap-6 items-center">
+                <div className="flex max-w-[518px] flex-col gap-6 items-center">
                     <p className="text-6xl font-bold text-center">Explore DEXs in orderbook format</p>
-                    <p className="text-center max-w-[648px] font-light">
+                    <p className="text-center max-w-[648px] font-light px-10">
                         Orderbook.wtf aggregates simulated trades over all DEX pools so you can see the shape of all liquidity in one orderbook.
                     </p>
                     <div className="flex gap-2">
@@ -55,93 +55,165 @@ export default function Page() {
                 </div>
 
                 {/* cards */}
-                <div className="grid grid-cols-1 lg:grid-cols-2 w-full gap-7 max-w-screen-xl sm:px-8">
-                    {/* 1 */}
-                    <FeatureCard
-                        text={
-                            <div className="flex gap-2 flex-col z-50">
-                                <p className="text-lg text-aquamarine">Orderbook</p>
-                                <p className="font-light text-lg max-w-[278px]">
-                                    On-chain liquidity visualized as discrete price-volume points, simulating real limit orders.
-                                </p>
-                            </div>
-                        }
-                        svg={
-                            <div className="w-[270px] absolute top-4 -right-4 rounded-xl p-2">
-                                <SvgWrapper className="w-[270px] h-80">
-                                    <Image src={'about-orderbook.svg'} alt={'orderbook'} fill className="rounded-xl" />
-                                </SvgWrapper>
-                            </div>
-                        }
-                    />
+                <div className="flex flex-col sm:px-8 gap-28 max-w-screen-xl">
+                    <div className="grid grid-cols-1 lg:grid-cols-2 w-full gap-7">
+                        {/* 1 */}
+                        <FeatureCard
+                            text={
+                                <div className="flex gap-2 flex-col z-50">
+                                    <p className="text-sm text-aquamarine">Orderbook</p>
+                                    <p className="font-light text-lg max-w-[278px]">
+                                        On-chain liquidity visualized as discrete price-volume points, simulating real limit orders.
+                                    </p>
+                                </div>
+                            }
+                            svg={
+                                <div className="w-[270px] absolute top-4 -right-4 rounded-xl p-2">
+                                    <SvgWrapper className="w-[270px] h-80">
+                                        <Image src={'about-orderbook.svg'} alt={'orderbook'} fill className="rounded-xl" />
+                                    </SvgWrapper>
+                                </div>
+                            }
+                        />
 
-                    {/* 2 */}
-                    <FeatureCard
-                        className="flex-col"
-                        text={
-                            <div className="flex gap-2 flex-col z-50">
-                                <p className="text-lg text-aquamarine">Depth</p>
-                                <p className="font-light text-lg">Aggregated buy/sell levels, reconstructed into a CEX-style chart.</p>
-                            </div>
-                        }
-                        svg={
-                            <div className="rounded-xl flex p-1 bg-background/50 mt-6">
-                                <SvgWrapper className="w-[576px] h-[215px]">
-                                    <Image src={'about-depth.svg'} alt={'depth'} fill className="rounded-xl" />
-                                </SvgWrapper>
-                            </div>
-                        }
-                    />
+                        {/* 2 */}
+                        <FeatureCard
+                            className="flex-col"
+                            text={
+                                <div className="flex gap-2 flex-col z-50">
+                                    <p className="text-sm text-aquamarine">Depth</p>
+                                    <p className="font-light text-lg">Aggregated buy/sell levels, reconstructed into a CEX-style chart.</p>
+                                </div>
+                            }
+                            svg={
+                                <div className="rounded-xl flex p-1 bg-background/50 mt-6">
+                                    <SvgWrapper className="w-[576px] h-[215px]">
+                                        <Image src={'about-depth.svg'} alt={'depth'} fill className="rounded-xl" />
+                                    </SvgWrapper>
+                                </div>
+                            }
+                        />
 
-                    {/* 3 */}
-                    <FeatureCard
-                        className="flex-col"
-                        text={
-                            <div className="flex gap-2 flex-col z-50">
-                                <p className="text-lg text-aquamarine">Routing</p>
-                                <p className="font-light text-lg">See how trades are split for best execution: pools used, amounts, and pathing.</p>
-                            </div>
-                        }
-                        svg={
-                            <div className="rounded-xl flex p-1 bg-background/50">
-                                <SvgWrapper className="w-[586px] h-[148px]">
-                                    <Image src={'about-routing.svg'} alt={'routing'} fill className="rounded-xl" />
-                                </SvgWrapper>
-                            </div>
-                        }
-                    />
+                        {/* 3 */}
+                        <FeatureCard
+                            className="flex-col"
+                            text={
+                                <div className="flex gap-2 flex-col z-50">
+                                    <p className="text-sm text-aquamarine">Routing</p>
+                                    <p className="font-light text-lg">
+                                        See how trades are split for best execution: pools used, amounts, and pathing.
+                                    </p>
+                                </div>
+                            }
+                            svg={
+                                <div className="rounded-xl flex p-1 bg-background/50">
+                                    <SvgWrapper className="w-[586px] h-[148px]">
+                                        <Image src={'about-routing.svg'} alt={'routing'} fill className="rounded-xl" />
+                                    </SvgWrapper>
+                                </div>
+                            }
+                        />
 
-                    {/* 4 */}
-                    <FeatureCard
-                        className="flex-col"
-                        text={
-                            <div className="flex gap-2 flex-col z-50">
-                                <p className="text-lg text-aquamarine">Liquidity</p>
-                                <p className="font-light text-lg">Explore TVL by pool, track where liquidity comes from and how it evolves.</p>
-                            </div>
-                        }
-                        svg={
-                            <div className="rounded-xl flex p-1 bg-background/50 w-[1000px] mt-10">
-                                <SvgWrapper className="w-[600px] h-[190px]">
-                                    <Image src={'about-liquidity.svg'} alt={'liquidity'} fill className="rounded-xl" />
-                                </SvgWrapper>
-                            </div>
-                        }
-                    />
-                </div>
+                        {/* 4 */}
+                        <FeatureCard
+                            className="flex-col"
+                            text={
+                                <div className="flex gap-2 flex-col z-50">
+                                    <p className="text-sm text-aquamarine">Liquidity</p>
+                                    <p className="font-light text-lg">Explore TVL by pool, track where liquidity comes from and how it evolves.</p>
+                                </div>
+                            }
+                            svg={
+                                <div className="rounded-xl flex p-1 bg-background/50 w-[1000px] mt-10">
+                                    <SvgWrapper className="w-[600px] h-[190px]">
+                                        <Image src={'about-liquidity.svg'} alt={'liquidity'} fill className="rounded-xl" />
+                                    </SvgWrapper>
+                                </div>
+                            }
+                        />
+                    </div>
 
-                {/* bottom */}
-                <div className="flex flex-col gap-4 items-center pb-20">
-                    <p className="text-4xl font-bold text-center">Run locally</p>
-                    <p className="text-center">How to locally run the frontend + backend</p>
-                    <LinkWrapper
-                        href={AppUrls.DOCUMENTATION}
-                        target="_blank"
-                        className="flex items-center gap-2 px-4 py-2.5 cursor-alias w-max bg-milk-100/5 hover:bg-milk-100/10 transition-colors duration-300 rounded-xl"
-                    >
-                        <p className="text-milk text-sm truncate">Docs</p>
-                        <IconWrapper icon={IconIds.OPEN_LINK_IN_NEW_TAB} className="size-4" />
-                    </LinkWrapper>
+                    {/* features */}
+                    <div className="flex flex-col max-w-screen w-full items-start gap-4">
+                        <p className="text-sm text-aquamarine">Features</p>
+                        <p className="text-4xl font-bold max-w-[588px] text-left">Everything Tycho Orderbook brings to your stack</p>
+                        <div className="grid grid-cols-1 lg:grid-cols-3 w-full gap-4 mt-10">
+                            {/* 1 */}
+                            <div className="flex flex-col p-6 cursor-alias transition-colors duration-300 rounded-xl gap-12 w-full bg-[#FFF4E005]">
+                                <div className="w-[350px] mx-auto">
+                                    <SvgWrapper className="w-[350px] h-[271px]">
+                                        <Image src={'./about-features.svg'} alt={'features'} fill className="rounded-xl" />
+                                    </SvgWrapper>
+                                </div>
+                                <div className="flex flex-col gap-3">
+                                    <p>See the real shape of onchain liquidity</p>
+                                    <p className="text-milk-400 text-base font-light">
+                                        The Tycho Orderbook SDK runs a solver to simulate hundreds of trades every block over all DEX pools. It then
+                                        aggregates the results into one orderbook, that combines the liquidity from all DEX pools and shows you the
+                                        true shape of available liquidity.
+                                    </p>
+                                </div>
+                            </div>
+                            {/* 2 */}
+                            <div className="flex flex-col p-6 cursor-alias transition-colors duration-300 rounded-xl gap-12 w-full bg-[#FFF4E005]">
+                                <div className="w-[350px] mx-auto">
+                                    <SvgWrapper className="w-[350px] h-[271px]">
+                                        <Image src={'./about-features.svg'} alt={'features'} fill className="rounded-xl" />
+                                    </SvgWrapper>
+                                </div>
+                                <div className="flex flex-col gap-3">
+                                    <p>Trade over DEXs like an orderbook</p>
+                                    <p className="text-milk-400 text-base font-light">
+                                        Not just a visual. Each point on the orderbook is a route and call data for the best split trade over DEXs
+                                        pools at this trade amount. Execute each point directly onchain.
+                                    </p>
+                                </div>
+                            </div>
+                            {/* 3 */}
+                            <div className="flex flex-col p-6 cursor-alias transition-colors duration-300 rounded-xl gap-12 w-full bg-[#FFF4E005]">
+                                <div className="w-[350px] mx-auto">
+                                    <SvgWrapper className="w-[350px] h-[271px]">
+                                        <Image src={'./about-features.svg'} alt={'features'} fill className="rounded-xl" />
+                                    </SvgWrapper>
+                                </div>
+                                <div className="flex flex-col gap-3">
+                                    <p>Run CeFi strategies on DeFi</p>
+                                    <p className="text-milk-400 text-base font-light">
+                                        If your backend is setup for orderbooks, you can now trade onchain like on any orderbook. Simply run the Tycho
+                                        Orderbook SDK locally, stream yourself the onchain orderbook, and execute trades.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    {/* links */}
+                    <div className="grid grid-cols-1 lg:grid-cols-3 w-full gap-4 pb-28">
+                        <LinkWrapper
+                            href={AppUrls.ORDERBOOK}
+                            target="_self"
+                            className="flex flex-col justify-between p-6 cursor-pointer bg-folly/95 hover:bg-folly/100 transition-colors duration-300 rounded-xl h-[156px]"
+                        >
+                            <IconWrapper icon={IconIds.OPEN_LINK_IN_NEW_TAB} className="size-6" />
+                            <p className="text-milk text-lg truncate">Docs</p>
+                        </LinkWrapper>
+                        <LinkWrapper
+                            href={AppUrls.DOCUMENTATION}
+                            target="_blank"
+                            className="flex flex-col justify-between p-6 cursor-alias bg-milk-100/5 hover:bg-milk-100/10 transition-colors duration-300 rounded-xl h-[156px]"
+                        >
+                            <IconWrapper icon={IconIds.OPEN_LINK_IN_NEW_TAB} className="size-6" />
+                            <p className="text-milk text-lg truncate">Read the docs</p>
+                        </LinkWrapper>
+                        <LinkWrapper
+                            href={AppUrls.PROPELLERHEADS_TELEGRAM}
+                            target="_blank"
+                            className="flex flex-col justify-between p-6 cursor-alias bg-milk-100/5 hover:bg-milk-100/10 transition-colors duration-300 rounded-xl h-[156px]"
+                        >
+                            <IconWrapper icon={IconIds.TELEGRAM_LOGO} className="size-6" />
+                            <p className="text-milk text-lg truncate">Join tycho.build</p>
+                        </LinkWrapper>
+                    </div>
                 </div>
             </div>
         </PageWrapper>
